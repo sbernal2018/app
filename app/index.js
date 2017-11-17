d5036cf9e191de01a26e87e5b18b720c1f0e1659
ex0dus-0x <ex0dus@codemuch.tech>
2017-11-17 10:15:21 -0500

    Project structure

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
-const exphbs  = require('express-handlebars');
 const express = require('express');
+const exphbs  = require('express-handlebars');
+
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const app = express();
@@ -16,7 +17,6 @@ app.engine('handlebars', exphbs({
   helpers: helpers
 }));
 
-
 app.set('view engine', 'handlebars');
 
 app.set('views', path.join(__dirname, './../views'))
@@ -24,16 +24,52 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+// Set bodyParser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
-function writeCompanyData(companyID, name, employees, imageUrl) {
+// Helpers for interfacing realtime database
+function writeCompanyData(companyID, employees, industry, location, imgUrl, description) {
   db.ref('companies/' + companyID).set({
-    name: name,
-    employees: employees, // array
-    profile_picture : imageUrl
+    employees: employees,
+    industry: industry,
+    location: location,
+    imgUrl: imgUrl,
+    description: description
   });
+}
+
+function writeCompanyEmployees(employeeID, position, profilePic, jobDescription, contact){
+  db.ref('employees/' + employeeID).set({
+    position: position,
+    profilePic: profilePic,
+    jobDescription: jobDescription,
+    contact: contact,
+  });
+}
+
+function checkIfCompanyExists(companyID) {
+  db.ref("companies/").child(companyID).once('value', function(snapshot) {
+    var exists = (snapshot.val() !== null);
+    companyExistsCallback(companyID, exists);
+  });
+}
+
+function companyExistsCallback(companyID, exists) {
+  if (exists) {
+    //console.log('Company ' + companyID + ' exists!');
+    return true;
+  } else {
+    //console.log('Company ' + companyID + ' does not exist!');
+    return false;
+  }
 }
 
 // Default search directory as index
@@ -41,6 +77,16 @@ app.get('/', (req, res) => {
   res.render("index", {
     title: "Search Directory",
   });
+});
+
+app.post('/', (req, res) => {
+  var company = req.body.company;
+
+  if ( checkIfCompanyExists(company) == false ) {
+    res.redirect('/');
+  } else {
+    res.redirect(`/search/${company}`);
+  }
 });
 
 // Standard static about page
@@ -53,6 +99,9 @@ app.get('/about', (req, res) => {
 // Search query to get entries back
 app.get('/search/:query', (req, res) => {
     const { query } = req.params;
+    res.render("search", {
+      title: `Search - ${query}`,
+    });
 
 });
 
@@ -70,27 +119,18 @@ app.get('/company/:query', (req, res) => {
 
 });
 
+// Employee name and information, all rendered in individual pages
 app.get('/profile/:name', (req, res) => {
     const { query } = req.params;
-
 
 });
 
 // Private admin page. Requires hard-coded admin password
 app.get('/admin', (req, res) => {
-  var invite_token = req.param('invite_token');
+  var invite_token = req.body.invite_token;
+
 });
 
-// API endpoint for retrieving all companies
-app.get('/api/companies', (req, res) => {
-  var ref = admin.database().ref("/companies");
-
-  ref.once("value", function(userSnapshot) {
-    userSnapshot.forEach(function(userSnapshot) {
-        res.send(userSnapshot.val());
-    });
-  });
-});
 
 
 // 404 Handler
@@ -100,7 +140,7 @@ app.use(function(req, res, next) {
     next(err);
 });
 
-// error handler
+// Error Handler
 app.use(function(err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
